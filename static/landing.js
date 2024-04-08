c933f9f94f7aef93e2bfc6a0b7402264e10d120b
Ryan J <19141558@brookes.ac.uk>
2024-04-08 13:11:49 +0100

    --EOD

--- static/landing.js
+++ static/landing.js
@@ -172,7 +172,7 @@ function logout() {
         url: '/logout',
         success: function(response) {
             // test logout success
-            console.log('Logout success:', response);
+            console.log('Logout success:');
             checkLoggedIn();
         },
         error: function(xhr, status, error) {
